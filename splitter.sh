d25112b66176d410ff00deb7f2e639fbae1487b5
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-09-28 15:25:59 -0400

    Add command arguments and defaults

--- splitter.sh
+++ splitter.sh
@@ -1,4 +1,16 @@
 vcf=${1}
+SPLITTER=${2}
+
+if [ $vcf -eq '']
+then
+    echo 'Please give vcf as first argument.'
+    exit
+fi
+
+if [ $SPLITTER -eq '']
+then
+    SPLITTER="/sb/project/ams-754-aa/zaf_projects/imputation/data/splitVCFref.jar"
+fi
 
 ### CHUNK THIS VCF
 MIN_AND_MAX=$(zcat ${vcf} | grep -v -E "^#" | awk '{print $2}' | sort |  awk 'NR==1; END{print}')
@@ -10,8 +22,5 @@ echo "Using min:${MIN} and max:${MAX}"
 # Calculates the actual size of a chunk in each VCF
 CORESIZE=5000000 # 5Mb region
 CHUNKSIZE=$(python -c "from math import ceil; range=${MAX}-${MIN}; chunks=ceil(range/float(${CORESIZE})); print(int(ceil(range / chunks)))") &&
-
-
-SPLITTER="/sb/project/ams-754-aa/zaf_projects/imputation/data/splitVCFref.jar"
 
 java -Xmx4g -jar $SPLITTER --vcfref ${vcf} --coresize ${CHUNKSIZE}.b
